dbb8a9f644b12f16ebdd18c8ee4bb8d4bbadff12
Joao Lapas <artesanatoavela@gmail.com>
2023-05-06 15:03:25 +0100

    fixing card loading bugs

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -27,6 +27,25 @@ const MovieCard = ({ id, path, title, rating, year }) => {
         </div>
       </div>
     );
+  } else {
+    return (
+        <div
+          className="h-80 relative"
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          
+          <div
+            className={`absolute top-0 h-full w-full text-center p-10 transition-all ease-in-out duration-900 bg-opacity-80 bg-neutral-900`}
+          >
+            <h3 className="font-bold text-xl h-3/4">{title}</h3>
+            <h5 className="text-red-500">{year}</h5>
+            <h5 className="text-red-500">
+              Rating: <span className="text-slate-50">{rating}</span>/10
+            </h5>
+          </div>
+        </div>
+    )
   }
 };
 
