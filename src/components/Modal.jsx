bf1208b71abe75e2947edbe469e98ace7223b3f5
Joao Lapas <artesanatoavela@gmail.com>
2023-05-06 14:33:53 +0100

    some visual updates

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -38,11 +38,12 @@ const Modal = ({ id, isOpen, handleCloseModal }) => {
 
   return (
     <div
-      onClick={() => handleCloseModal()}
-      className="flex flex-col items-center justify-center fixed bg-black bg-opacity-80 rounded-3xl z-30 top-0 bottom-0 right-0 left-0 "
+      className="flex overflow-scroll flex-col items-center justify-center fixed bg-black bg-opacity-80 rounded-3xl z-30 top-0 bottom-0 right-0 left-0 "
     >
-      <div className="flex overflow-scroll flex-col items-center absolute bg-neutral-800 rounded-3xl z-30 sm:w-2/3 sm:h-3/4 md:h-5/6 md:w-3/4 py-10">
-        <div className="flex items-center justify-between flex-wrap h-5/6 w-10/12 mt-28 lg:mt-0">
+    
+      <div className="flex flex-col items-center absolute bg-neutral-800 rounded-3xl z-30 sm:w-2/3 sm:h-3/4 md:h-5/6 md:w-3/4 py-10">
+      <div onClick={() => handleCloseModal()} className='fixed text-4xl text-slate-50 z-50 top-5 right-5'>x</div>  
+      <div className="flex items-center justify-between flex-wrap h-5/6 w-10/12 mt-28 lg:mt-0">
           <div className="w-11/12 sm:w-6/12 text-center md:text-left pl-8">
             <h1 className="text-2xl sm:text-4xl text-slate-50 mb-10">
               {details.original_title}
